cf250ff86bde7608003d5175d5d9579ff7e787d5
osamanisar05 <164087967+osamanisar05@users.noreply.github.com>
2024-11-10 19:32:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Get references to the form and dispaly area
+var form = document.getElementById('Resume-Form');
+var resumeDispalyElement = document.getElementById('resumeDisplay');
+//Handle form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //prevent page reload
+    //collect input values
+    //type assertion
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //create resume output
+    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>skills</h3>\n    <p>").concat(skills, "</p>\n\n    ");
+    //dispaly the generated resume
+    if (resumeDispalyElement) {
+        resumeDispalyElement.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume desplay element is missing');
+    }
+});
